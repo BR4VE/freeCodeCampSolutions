563bbb4a9b47a662160226482aab7fb8997ce3de
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2018-12-25 16:51:38 +0300

    arrow to normal

--- Intermediate_Algorithm_Scripting/diff_two_arrays_es5.js
+++ Intermediate_Algorithm_Scripting/diff_two_arrays_es5.js
@@ -1,14 +1,14 @@
 function diffArray(arr1, arr2) {
   var newArr = [];
   // loop through the first arr
-  arr1.forEach(element => {
+  arr1.forEach(function(element) {
     // if the second arr and newarr do not contain the element add it to the newArr
     if(arr2.indexOf(element) < 0 && newArr.indexOf(element) < 0) {
       newArr.push(element);
     }
   });
   // do same for the second array
-  arr2.forEach(element => {
+  arr2.forEach(function(element) => {
     if(arr1.indexOf(element) < 0 && newArr.indexOf(element) < 0) {
       newArr.push(element);
     }
